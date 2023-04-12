5aa288be24c8325b9ce9a11ffd9db6328520c2cc
ibroximovraxmatulloh <ibroximovraxmatulloh69@gmail.com>
2023-04-12 15:28:30 +0500

    yubordim

--- is_even.py
+++ is_even.py
@@ -1,7 +1,7 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
-number = 21
-answer = number %2
+number = 22
+answer = (number+1) %2
 
 print(answer)
