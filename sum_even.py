8e83c4ba485adffdae354f5c0a0a317e907f0188
ibroximovraxmatulloh <ibroximovraxmatulloh69@gmail.com>
2023-04-17 18:17:08 +0500

    yubordim

--- sum_even.py
+++ sum_even.py
@@ -4,3 +4,14 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+
+var_int = 1876
+sum_even = 0
+
+x1 = var_int //1000
+x2 = var_int //100%10
+x3 = var_int %100//10
+x4 = var_int %10
+sum_even = ((x1+1)%2*x1+(x2+1)%2*x2+(x3+1)%2*x3+(x4+1)%2*x4)
+
+print(sum_even)
